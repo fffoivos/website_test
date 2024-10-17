15d66444c8816121d500606f4b2848f6cce988a5
fffoivos <foivos.karounos@gmail.com>
2024-10-17 09:40:46 +0300

    reorg

--- manual_cleaning_excercises/copy_paste_cleaning/sxolika/copy_paste_cleaning_first_cleaning.py
+++ manual_cleaning_excercises/copy_paste_cleaning/sxolika/copy_paste_cleaning_first_cleaning.py
@@ -4,18 +4,19 @@ import unicodedata
 import math
 
 # Define the directory paths
-input_directory = '/home/fivos/Desktop/Projects/GlossAPI/raw_txt/sxolika/paste_texts'
+input_directory = '/home/fivos/Projects/GlossAPI/raw_txt/sxolika/paste_texts'
 # Outputs here:
-output_directory = os.path.join(input_directory, 'cleaning_presentation')
+output_directory = os.path.join(input_directory, 'xondrikos_katharismos_papers')
 
 # Create the output directory if it doesn't exist
 os.makedirs(output_directory, exist_ok=True)
 
 # Intended match: Greek_words, optional space, more than 3 dots, optional space and/or "σελ.", finally a number and nothing else concatenated to it.
 #   eg 'ΠΡΟΚΑΤΑΡΚΤΙΚΕΣ ΕΡΓΑΣΙΕΣ ΣΥΝΤΗΡΗΣΗΣ ........................................................50'
-index_pattern = re.compile(r"^([Α-Ω]\. )?((\d{1,2}\.)*(\d)?)? {0,4}(\.*)\s{0,2}[Α-ΩΆ-ΏA-Z](([a-zA-Zα-ωΑ-Ωά-ώ-.:])+ \d{0,2})+\s{0,4}(\.)*(\s{0,2}(σελ\.|Σελ\.\:|))?\s{0,4}\d+$")
-
+index_pattern = re.compile(r"([α-ωa-zA-Α-ΩΆ-Ώά-ώ \d:]+)\s{0,4}(\.{4,})\s{0,2}(σελ\.|Σελ\.\:|«)?\s{0,4}\d+(?!\S)")
+index_without_dots = re.compile(r"([Α-ΩΆ-ΏΪΫ– ])+ {1,2}\d{1,3}$")
 # Pattern for bibliography
+bibliography_with_fullstop = re.compile(r".*βιβλιογραφια\.$")
 bibliography_pattern = re.compile(r".*βιβλιογραφια.*")
 legal_statement_pattern = re.compile(r".*Βάσει του ν\. 3966/2011 τα διδακτικά βιβλία.*", re.IGNORECASE)
 pagination_pattern = re.compile(r"^((\d){1,2}|(. (\d) .)|\[(\d)\]|(vi{0,3}))$")
@@ -30,6 +31,11 @@ def find_bibliography_line(line):
         return match
     else:
         return False
+    
+def not_with_fullstop(line):
+    accentless_line = ''.join(c for c in unicodedata.normalize('NFD', line.lower()) if unicodedata.category(c) != 'Mn')
+    if bibliography_with_fullstop.match(accentless_line): return False
+    else: return True
 
 def find_legal_statement_line(line):
     # Check if the line matches the legal statement pattern
@@ -51,10 +57,12 @@ def find_page_number(line):
     return 0
 
 def process_file(file_path):
-    last_index_line_number = 0
+    intro_cutoff_point = 0
+    last_index_dottedline_number = 0
+    last_index_undottedline_number = 0
     bibliography_line_number = None
     legal_statement_line_number = None
-    page_number = 0  # To store the line number of the page seven match
+    page_number = 0  # Read pages variable
     page_number_line = 1
 
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -66,19 +74,20 @@ def process_file(file_path):
         
         for line_number, line in enumerate(lines, 1):
             # First, check for index entries (within the first half of the text)
-            if (line_number / txt_length) < 0.1:
+            if line_number < 400:
                 if index_pattern.search(line):
-                    last_index_line_number = line_number
-                
+                    last_index_dottedline_number = line_number
+                if index_without_dots.search(line):
+                    last_index_undottedline_number = line_number
                 # Simultaneously check for the seventh page patterns and store the line if found
                 if page_number not in {7, 8}:
                     new_page_number = find_page_number(line)
-                    if new_page_number - page_number > 0:
+                    if new_page_number - page_number > 0 and new_page_number < 9:
                         page_number = new_page_number
                         page_number_line = line_number
             
             # Check for bibliography after 90% of the document
-            if (line_number / txt_length) > 0.9 and find_bibliography_line(line):
+            if (line_number / txt_length) > 0.9 and find_bibliography_line(line) and not_with_fullstop(line):
                 bibliography_line_number = line_number - 1
                 break  # Stop processing after finding bibliography
             
@@ -87,10 +96,16 @@ def process_file(file_path):
                 bibliography_line_number = line_number - 1
         
         # If no index pattern was found, fall back to the seventh page line number if it exists
-        if last_index_line_number == 0 and page_number_line is not None:
-            last_index_line_number = page_number_line
-    
-    return last_index_line_number, bibliography_line_number, lines
+        if last_index_dottedline_number:
+            intro_cutoff_point = last_index_dottedline_number
+        elif last_index_undottedline_number:
+            intro_cutoff_point = last_index_undottedline_number
+        elif page_number_line is not None:
+            intro_cutoff_point = page_number_line
+    if intro_cutoff_point < 2:
+        #print(os.path.basename(file_path))
+        pass
+    return intro_cutoff_point, bibliography_line_number, lines
 
 def print_presentation(file_distances):
     # Calculate the top x% files with the longest distances
@@ -119,7 +134,6 @@ def main():
             continue
         
         # Process the file
-        print(f"Processing {filename}")
         last_index_line_number, bibliography_line_number, lines = process_file(file_path)
         
         txt_length = len(lines)
@@ -134,28 +148,17 @@ def main():
         for line_number, line in enumerate(lines, 1):
             if line_number == last_index_line_number + 1:
                 # Start of content
-                output_lines.append("[=== ΑΡΧΉ ΚΑΘΑΡΙΣΜΈΝΟΥ ΑΡΧΕΊΟΥ ===]\n")
                 inside_content = True
             if line_number == bibliography_line_number + 1:
                 # End of content
-                output_lines.append("[=== ΤΈΛΟΣ ΚΑΘΑΡΙΣΜΈΝΟΥ ΑΡΧΕΊΟΥ ===]\n")
                 inside_content = False
             if inside_content:
                 output_lines.append(line)
-            else:
-                output_lines.append("[ΕΚΤΟΣ] " + line)
-        
-        # If content goes till the end of file, ensure we add the end marker
-        if inside_content:
-            output_lines.append("[=== ΤΈΛΟΣ ΚΑΘΑΡΙΣΜΈΝΟΥ ΑΡΧΕΊΟΥ ===]\n")
-        
+                
         # Write the transformed lines to the output file
         output_file_path = os.path.join(output_directory, filename)
         with open(output_file_path, 'w', encoding='utf-8') as output_file:
             output_file.writelines(output_lines)
-        
-        #print_presentation(file_distances)
-    
 
 if __name__ == '__main__':
     main()
